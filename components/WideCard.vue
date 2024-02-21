cdfab9ae36d22a87d1823ab1154e54d225da595a
ivanLFN <LFN.current@yandex.com>
2024-02-21 13:27:18 +0700

    feat(ThinCard.vue, WideCard.vue): adding new card components for article list

---
+++ components/WideCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <client-only>
+      <NuxtLink :to="{ name: 'blog-slug', params: { slug: article.slug } }">
+        <img :src="article.img" />
+        <div>
+          <h2>{{ article.title }}</h2>
+          <p>by {{ article.author.name }}</p>
+          <p>{{ article.description }}</p>
+        </div>
+      </NuxtLink>
+    </client-only>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    article: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
