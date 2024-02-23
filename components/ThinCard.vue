28d017ae115e1cf8e6b720579536e5f85d8cb046
ivanLFN <LFN.current@yandex.com>
2024-02-23 17:30:14 +0700

    feat(ThinCard.vue): addig a new style for Thin Card

--- components/ThinCard.vue
+++ components/ThinCard.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
     <client-only>
-      <NuxtLink :to="{ name: 'blog-slug', params: { slug: article.slug } }">
-        <img :src="article.img" />
-        <div>
-          <h2>{{ article.title }}</h2>
-          <p>by {{ article.author.name }}</p>
-          <p>{{ article.description }}</p>
+      <NuxtLink :to="{ name: 'blog-slug', params: { slug: article.slug } }" class="parent-link nuxt-link-wrapper">
+        <div class="img-style">
+          <img :src="`/articles/${article.img}`" class="card-image" />
+        </div>
+        <div class="content p-3">
+          <p class="created-at-style">{{ formatDate(article.createdAt) }}</p>
+          <div>
+            <div class="article-title">{{ article.title }}</div>
+          </div>
+          <!-- <p>{{ article.description }}</p> -->
         </div>
       </NuxtLink>
     </client-only>
@@ -20,6 +24,73 @@ export default {
       type: Object,
       required: true
     }
+  },
+  methods: {
+    formatDate(date) {
+      const options = { year: 'numeric', month: 'long', day: 'numeric' }
+      return new Date(date).toLocaleDateString('en', options)
+    }
   }
 }
 </script>
+
+<style scoped>
+
+.article-title {
+  color: #272727;
+  text-decoration: none;
+  font-weight: 700;
+  font-size: 1.5rem;
+  transition: color 1s ease;
+}
+
+.created-at-style {
+  color: #4f4f4f;
+}
+
+.nuxt-link-wrapper .card-image {
+  transition: transform 1s ease;
+}
+
+.nuxt-link-wrapper:hover .card-image {
+  transform: scale(1.1);
+  transition: transform 1s ease;
+}
+
+.nuxt-link-wrapper:hover {
+  text-decoration: none;
+}
+
+.nuxt-link-wrapper:hover .article-title {
+  color: #7c4dff;
+}
+
+.parent-link {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+
+.img-style {
+  flex: 1;
+  overflow: hidden;
+}
+
+.content {
+  flex: 1;
+}
+
+.img-style img {
+  height: 100%;
+  width: 100%;
+}
+
+.card-image {
+  display: block;
+  margin: 0 auto;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+</style>
